3a5b6a0d0437cc4c3e44539978f4cbe01d036375
Unai Navarro <165326999+CyberRangeUser@users.noreply.github.com>
2024-12-12 09:10:59 +0100

    Add files via upload

---
+++ 15.1/fichero.js
@@ -0,0 +1,62 @@
+
+//window.open("http://www.google.es", "Google","heigth=600px, width=600px, menubar=no, resizable=no");
+
+/*if (window.confirm("¿Está seguro que desea continuar?")){
+    alert ("El usuario ha decidido continuar");
+    // instrucciones a ejecutar en caso de respuesta afirmativa
+}
+else{
+    alert("El usuario ha decidido cancelar");
+    //instrucciones a ejecutar en caso de respuesta negativa
+}
+    
+   */ 
+
+/*var respuesta=prompt("Introduzca el título de su película preferida", "");
+//la variable respuesta almacenará la respuesta del usuario
+alert ("Su película preferida es: "+respuesta);
+
+
+var servidor=location.host;
+alert("El servidor de esta página web es: "+servidor);
+
+
+var urlActual=location.href;
+alert("La URL actual es: "+urlActual+", a continuación haremos una redirección a google.es");
+location.href="http://www.google.es";
+
+
+*/
+var appCodeName=navigator.appCodename;
+var appName=navigator.appName;
+var appVersion=navigator.appVersion;
+var platform=navigator.platform;
+var userAgent=navigator.userAgent;
+var cookieEnabled=navigator.cookieEnabled;
+var javaEnabled=navigator.javaEnabled();
+document.write("* appCodename: "+appCodeName+"<br>");
+document.write("* appName: "+appName+"<br>");
+document.write("* appVersion: "+appVersion+"<br>");
+document.write("* platform:"+platform+"<br>");
+document.write("* userAgent: "+userAgent+"<br>");
+document.write("* Coockies activadas:" +cookieEnabled+"<br>");
+document.write("*Java activado:"+javaEnabled+"<br>");
+
+let log = console.log(window);
+document.write(log+"<br>");
+let direccion = window.location.href;
+document.write(direccion+"<br>");
+let atras= window.history.back();
+document.write(atras+"<br>");
+//window.prompt("Introduce tu edad");
+window.setTimeout(window.prompt("Introduce tu nombre"), 9000);
+//window.close();
+
+
+
+
+
+
+
+
+
